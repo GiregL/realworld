9c4c9fe7abdfb1ac8413378b8ddd222d6d3f34f0
Lezenn <lezenns@gmail.com>
2021-10-14 16:55:15 +0200

    Created first controller routes

---
+++ lib/realworld_web/controllers/users_controller.ex
@@ -0,0 +1,28 @@
+defmodule RealworldWeb.UsersController do
+  use RealworldWeb, :controller
+
+  @moduledoc """
+  Users controller
+
+  It is used to manage users and expose those endpoints:
+
+  GET /api/users/                       Get all users
+  GET /api/users/:userId                Get a user by its id
+  """
+
+  alias Realworld.{Repo, Realworld.User}
+
+  @doc """
+  Returns a list of all the users from the database
+  """
+  def user_list(conn, _params) do
+    json conn, Repo.all(User)
+  end
+
+  @doc """
+  Returns the user with the given id
+  """
+  def user_by_id(conn, params) do
+    conn
+  end
+end
