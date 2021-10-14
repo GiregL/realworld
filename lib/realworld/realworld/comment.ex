ed702e41323171a1f8c154b41e42fd2229d19bf1
Lezenn <lezenns@gmail.com>
2021-10-14 16:51:55 +0200

    Article reference

--- lib/realworld/realworld/comment.ex
+++ lib/realworld/realworld/comment.ex
@@ -5,13 +5,14 @@ defmodule Realworld.Realworld.Comment do
   schema "comments" do
     field :body, :string
     field :author, :id
+    field :article, :id
     timestamps()
   end
 
   @doc false
   def changeset(comment, attrs) do
     comment
-    |> cast(attrs, [:body, :author])
-    |> validate_required([:body, :author])
+    |> cast(attrs, [:body, :author, :article])
+    |> validate_required([:body, :author, :article])
   end
 end
