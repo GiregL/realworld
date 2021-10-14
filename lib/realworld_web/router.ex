3fa3df89d0883efd2b4aede209aa9ec6be22972e
Lezenn <lezenns@gmail.com>
2021-10-14 16:54:18 +0200

    Created all users and by id User routes

--- lib/realworld_web/router.ex
+++ lib/realworld_web/router.ex
@@ -1,6 +1,9 @@
 defmodule RealworldWeb.Router do
   use RealworldWeb, :router
 
+  #
+  # Pipelines
+  #
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -14,24 +17,30 @@ defmodule RealworldWeb.Router do
     plug :accepts, ["json"]
   end
 
+  #
+  # Basic scope
+  #
   scope "/", RealworldWeb do
     pipe_through :browser
 
     get "/", PageController, :index
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", RealworldWeb do
-  #   pipe_through :api
-  # end
-
-  # Enables LiveDashboard only for development
   #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
+  # Scope API
+  #
+  scope "/api", RealworldWeb do
+    pipe_through :api
+
+    #
+    # Users routes
+    get "/users/", UsersController, :user_list
+    get "/users/:userId", UsersController, :user_by_id
+  end
+
+  #
+  # Dashboard (Dev mode only)
+  #
   if Mix.env() in [:dev, :test] do
     import Phoenix.LiveDashboard.Router
 
