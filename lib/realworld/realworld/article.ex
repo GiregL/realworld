613d29d550eacbe6025440e5fbff3e5129030b0f
Lezenn <lezenns@gmail.com>
2021-10-14 15:18:09 +0200

    basic article schema creation (no relationships)

---
+++ lib/realworld/realworld/article.ex
@@ -0,0 +1,20 @@
+defmodule Realworld.Realworld.Article do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "articles" do
+    field :article_id, :integer
+    field :body, :string
+    field :description, :string
+    field :title, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(article, attrs) do
+    article
+    |> cast(attrs, [:title, :description, :body, :article_id])
+    |> validate_required([:title, :description, :body, :article_id])
+  end
+end
---
+++ priv/repo/migrations/20211014131717_create_articles.exs
@@ -0,0 +1,14 @@
+defmodule Realworld.Repo.Migrations.CreateArticles do
+  use Ecto.Migration
+
+  def change do
+    create table(:articles) do
+      add :title, :string
+      add :description, :string
+      add :body, :string
+      add :article_id, :integer
+
+      timestamps()
+    end
+  end
+end
