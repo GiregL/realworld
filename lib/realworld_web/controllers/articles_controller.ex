3097bbbf59c87ea6a100b18913e9b863e5a29683
Lezenn <lezenns@gmail.com>
2021-10-15 18:49:17 +0200

    [Bugged] New route to create an article. Needs to be fixed: only the title, description and body are inserted

--- lib/realworld_web/controllers/articles_controller.ex
+++ lib/realworld_web/controllers/articles_controller.ex
@@ -76,4 +76,102 @@ defmodule RealworldWeb.ArticlesController do
     end
   end
 
+  @doc """
+  Creates a new article
+  Required fields: title, description, body
+  Optional fields: tagList
+  """
+  def create_article(conn, %{"article" => params}) do
+    tagList = Map.get(params, "tagList", [])
+
+    #
+    # If the required keys are not found
+    if not Map.has_key?(params, "title")
+      or not Map.has_key?(params, "description")
+      or not Map.has_key?(params, "body") do
+
+      conn
+      |> put_status(400)
+      |> json(%{"error" => "Required fields not found: title, description, body"})
+
+    #
+    # If the required keys are found
+    else
+
+      base_article = %Article{
+        title: Map.get(params, "title"),
+        description: Map.get(params, "description"),
+        body: Map.get(params, "body")
+      }
+
+      {:ok, pid} = Agent.start_link(fn -> base_article end)
+
+      # TODO: BUG DE MISE A JOUR DE LA MAP EN FONCTION DES DONNEES FOURNIES
+      params
+      |> Enum.each(fn {key, value} ->
+        if Map.has_key?(base_article, key) do   # If the key is found in the query
+
+          Agent.get_and_update(pid, fn article ->
+            Map.put(article, key, value)
+          end)
+          |> IO.inspect
+        end
+      end)
+
+      article = Agent.get(pid, &(&1))
+      IO.inspect(article)
+
+      Agent.stop(pid)
+
+      case Repo.insert(article) do
+        {:ok, inserted} ->
+          article_id = inserted.id
+
+          #
+          # Adding all the tags to the new article
+          taglist_insertion = tagList
+          |> Enum.map(fn tag ->
+            # Adding the tags
+            tag_id = Repo.get_by(Tag, title: tag)
+
+            if is_nil(tag_id) do
+              {:error, "Tag: " <> tag <> " does not exists."}
+            else
+              case Repo.insert(%TagArticle{tag: tag_id.id, article: article_id}) do
+                {:ok, _} -> :ok
+                {:error, _} -> {:error, "Failed to add tag " <> tag <> " to article " <> article_id}
+              end
+            end
+          end)
+          |> Enum.all?(fn x ->
+            # Checking if all tags are added
+            case x do
+              :ok -> true
+              {:error, message} ->
+                IO.puts message
+                false
+            end
+          end)
+
+          # If a insertion fail occured
+          if taglist_insertion do
+            conn
+            |> put_status(201)
+            |> json(%{"success" => "Article added."})
+          else
+            conn
+            |> put_status(500)
+            |> json(%{"error" => "An error occured during tag association to the new article"})
+          end
+
+        {:error, _} ->
+          conn
+          |> put_status(500)
+          |> json(%{"error" => "Internal error, failed to insert new article."})
+      end
+    end
+  end
+
+  def create_article(conn, _params), do: conn |> put_status(400) |> json(%{"error" => "Invalid request, no article found."})
+
 end
